c00d8418ebbdd14ae833dfb42f7e2b246f7876be
Greg Pyatt <greg.pyatt@gmail.com>
2018-07-06 19:31:21 -0700

    Attempt to synch AZALIA & MainBox

--- Scratch/Exploring books to donate.sql
+++ Scratch/Exploring books to donate.sql
@@ -12,4 +12,7 @@ UPDATE ScannedBooks SET OKToDonate=1 WHERE BookID IN(
 719, 652, 9, 122, 526, 560, 452, 141, 139, 563, 587, 117, 
 60, 173, 370, 14, 89, 664, 248, 681, 131, 600, 523, 274, 
 100, 273, 374, 702, 564, 118, 618, 704, 115, 283
-)
+)  --34 books updated
+
+SELECT BookID, Title FROM ScannedBooks WHERE OKToDonate=1
+--34 books returned.
